d72a688cdcf1794bf63c889ac591a535daec43b1
Minh Vu <mdvu15@earlham.edu>
2023-04-12 12:48:45 -0500

    Modify demand constraint, add results json file

--- VU_HWK_3_OPT.py
+++ VU_HWK_3_OPT.py
@@ -55,7 +55,7 @@ model.OBJ = Objective(rule=obj_expression)
 
 # supply/demand match constraint
 def match_const(model, i):
-    return model.solar[i]*model.cap['s_cap'] +  model.wind[i]*model.cap['w_cap'] + model.ESS_d[i] - model.ESS_c[i] - model.curt[i] - demand == 0   
+    return model.solar[i]*model.cap['s_cap'] +  model.wind[i]*model.cap['w_cap'] + model.ESS_d[i] - model.ESS_c[i] - model.curt[i] - model.demand[i] == 0   
 model.match = Constraint(model.t, rule = match_const)
 
 # ESS charge/discharge constraint
